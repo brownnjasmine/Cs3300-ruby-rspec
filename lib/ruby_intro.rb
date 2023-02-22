6d9e23a4ebdf5b487c6fa8c133fb7a1b1288c616
brownnjasmine <brown.n.jasmine@gmail.com>
2023-02-22 03:24:21 +0000

    Started part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -13,30 +13,53 @@ end
 
 def max_2_sum arr
   # YOUR CODE HERE
-  num1 = 0
-  num2 = 0
-
-  if arr.empty
-    return num1
-  elsif arr.count == 1
-    num1 = arr.first
-    return num1
+  if arr.empty?
+    return 0
   end
+
+  if arr.length == 1
+    return arr[0]
+  end
+
+  arr.sort!
+  return arr.pop + arr.pop
 
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+  if arr.empty? || arr.length == 1
+    return false
+  end
+
+  for i in arr
+    for j in arr
+      if i + j == n && i != j
+        return true
+      end
+    end
+  end
+
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{ name }"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+
+  #if s[0] =~ 
+
+  if s[0] =~ /[^aeiou]/
+    return true;
+  end
 end
 
 def binary_multiple_of_4? s
